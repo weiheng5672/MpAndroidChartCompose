a7345126701140f8ab42aee6cd500ebdce68e21f
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2024-08-06 18:55:27 +0800

    折線圖與長條圖

--- app/src/main/java/com/example/mpandroidchartcompose/ui/MyLineChart.kt
+++ app/src/main/java/com/example/mpandroidchartcompose/ui/MyLineChart.kt
@@ -20,6 +20,7 @@ import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.ValueFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -53,12 +54,9 @@ fun MyLineChart(
                 // 创建 LineDataSet 和 LineData
                 val lineDataSet = LineDataSet(lineEntriesList, "Line Chart Data").apply {
                     color = ContextCompat.getColor(context, R.color.purple_200)
-                    valueTextColor = Color.RED
-                    valueTextSize = 25f
                     lineWidth = 2.5f
                     setDrawCircles(true)
                     setDrawCircleHole(false)
-                    circleRadius = 5f
                     setDrawValues(false)
                 }
 
@@ -74,6 +72,8 @@ fun MyLineChart(
                     xAxis.isEnabled = true
                     xAxis.setDrawLabels(true)
                     xAxis.position = XAxis.XAxisPosition.BOTTOM
+                    xAxis.valueFormatter = CustomYearValueFormatter()
+                    axisLeft.valueFormatter = CustomEnergyValueFormatter()
                     invalidate() // 刷新图表
                 }
 
@@ -81,6 +81,20 @@ fun MyLineChart(
             }
         )
 
+    }
+}
+
+class CustomEnergyValueFormatter : ValueFormatter() {
+    override fun getFormattedValue(value: Float): String {
+        // 自定义格式化逻辑，例如将值除以 1,000,000,000 并添加 "G" 单位
+        return "${value / 1_000_000_000}G"
+    }
+}
+
+class CustomYearValueFormatter : ValueFormatter() {
+    override fun getFormattedValue(value: Float): String {
+        // 不使用千位分隔符，直接将数字转为整数格式显示
+        return value.toInt().toString()
     }
 }
 
